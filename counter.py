32e58103a47e014fb2b158e8c33d32e3c8b60a97
Paolo Zaffino <p.zaffino@unicz.it>
2014-09-28 23:57:32 +0200

    Add green count and improve code organization

--- counter.py
+++ counter.py
@@ -4,17 +4,35 @@ import scipy.misc
 from scipy import ndimage
 import sys
 
-im1 = scipy.misc.imread(sys.argv[1])
+# Define utility function
+def remove_edge(im, num_pixel):
+    return im[num_pixel:-num_pixel, num_pixel:-num_pixel]
 
-blu = im1[:,:,2]
+def threshold_image(im, threshold):
+    im[im<=threshold]=0
+    return im
 
-blu_no_edge = blu[10:-10,10:-10]
+def count_cell(img, edge_pixels, threshold, erosion_iterations):
+    img_no_edge = remove_edge(img, edge_pixels)
+    img_thr = threshold_image(img_no_edge, threshold)
+    eroded_img = ndimage.morphology.binary_erosion(img_thr, iterations = erosion_iterations)
+    labels, number_labels = ndimage.label(eroded_img)
+    return number_labels
+   
+# Define common stuff
+edge_pixels = 10
+threshold = 50
+erosion_iterations = 4
 
-blu_no_edge[blu_no_edge<=50]=0
+# Read image and split channels
+cell_image = scipy.misc.imread(sys.argv[1])
+red, green, blu = [cell_image[:,:,i] for i in range(3)]
 
-eroded_blu = ndimage.morphology.binary_erosion(blu_no_edge,iterations=4)
+# Count 
+number_blu_labels = count_cell(blu, edge_pixels, threshold, erosion_iterations)
+number_green_labels = count_cell(green, edge_pixels, threshold, erosion_iterations)
 
-labels, nb = ndimage.label(eroded_blu)
-
-print (nb)
+# Print results
+print ("Blu cells = %d" % number_blu_labels)
+print ("Green cells = %d" % number_green_labels)
 
