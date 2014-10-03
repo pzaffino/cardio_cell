586e8687bfd3f5b1c864b436ebeb0764674ca837
Salvatore Scaramuzzino <salvatorescaramuzzino@gmail.com>
2014-10-03 22:51:02 +0200

    added ipython notebbok

--- counter.py
+++ counter.py
@@ -4,6 +4,13 @@ import scipy.misc
 from scipy import ndimage
 import sys
 
+from skimage.morphology import label
+from skimage.measure import regionprops
+
+from skimage.filter import threshold_otsu
+
+import matplotlib.pylab as plt
+
 # Define utility functions
 def remove_edge(im, num_pixel):
     return im[num_pixel:-num_pixel, num_pixel:-num_pixel]
@@ -12,16 +19,18 @@ def threshold_image(im, threshold):
     im[im<=threshold]=0
     return im
 
-def count_cell(img, edge_pixels, threshold, erosion_iterations):
+def count_cell(img, edge_pixels, erosion_iterations):
     img_no_edge = remove_edge(img, edge_pixels)
+    threshold = threshold_otsu(img_no_edge)
     img_thr = threshold_image(img_no_edge, threshold)
     eroded_img = ndimage.morphology.binary_erosion(img_thr, iterations = erosion_iterations)
     labels, number_labels = ndimage.label(eroded_img)
     return number_labels
-   
+
+    
+    
 # Define common stuff
 edge_pixels = 10
-threshold = 50
 erosion_iterations = 4
 
 # Read image and split channels
@@ -29,7 +38,7 @@ cell_image = scipy.misc.imread(sys.argv[1])
 red, green, blu = [cell_image[:,:,i] for i in range(3)]
 
 # Count 
-number_blu_labels = count_cell(blu, edge_pixels, threshold, erosion_iterations)
+number_blu_labels = count_cell(blu, edge_pixels, erosion_iterations)
 number_green_labels = count_cell(green, edge_pixels, threshold, erosion_iterations)
 
 # Print results
